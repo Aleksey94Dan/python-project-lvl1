43161b043c4f8df5678d1bc46d5578b9f6898ee7
Aleksey Danilchenko <danilchenko.aleksey94@gmail.com>
2019-09-29 22:15:20 +0700

    Init

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,13 +3,15 @@
 from .brain_games import out_text
 from brain_games.rule import rule
 from brain_games.cli import run
+from brain_games.logic import point, ATTEMPTS
 
 
 def main():
     out_text()
     rule()
     print()
-    run()
+    name = run()
+    point(ATTEMPTS, name)
 
 
 if __name__ == '__main__':

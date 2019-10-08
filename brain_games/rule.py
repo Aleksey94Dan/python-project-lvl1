1d11d9e2de8148507930477e1e172fa705a19a2a
Aleksey94Dan <danilchenko.aleksey94@gmail.com>
2019-10-08 21:15:50 +0700

    corected

--- brain_games/rule.py
+++ brain_games/rule.py
@@ -1,18 +1,7 @@
 
-# Констаннты, задающие цвет тексту
-# Цвета для rule
-RED_RULE = '\033[91m'
-RED_PRIME = '\033[91m'
-BLUE_PRIME = '\033[34m'
-END_RULE = '\033[0m'
-END_PRIME = '\033[0m'
-
 
 def rule():
-    print("Answer" +
-          RED_RULE + " \"yes\" " + END_RULE +
-          "if number even otherwise answer" +
-          RED_RULE + " \"no\"" + END_RULE + ".")
+    print("Answer \"yes\" if number even otherwise answer \"no\".")
     print()
 
 
@@ -32,16 +21,18 @@ def question_progression():
 
 
 def question_prime():
-    print('Answer ' + RED_PRIME + " \"yes\" " + END_PRIME +
-          ' if given ' + BLUE_PRIME + 'number' + END_PRIME +
-          ' is prime. Otherwise answer ' +
-          RED_PRIME + " \"no\"" + END_PRIME + '.')
+    print('Answer  \"yes\" if given number is prime. Otherwise answer '
+          '  \"no\".')
     print()
 
 
-if __name__ == '__main__':
+def main():
     rule()
     question_calc()
     question_gcd()
-    question_progression()
     question_prime()
+    question_progression()
+
+
+if __name__ == '__main__':
+    main()

d345f96ee8db8cc0c879cb985ca3052599d790f0
Aleksey Danilchenko <danilchenko.aleksey94@gmail.com>
2019-10-21 23:22:34 +0700

    fix

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -26,17 +26,21 @@ def check_response(answer, name, data):
 
 
 def run(game=None):
-    greet()
-    game.get_denotation()
-    username = cli.get_username()
-    i = 0
-    while i < ATTEMPTS:
-        game_issue, game_response = game.get_game()
-        ask(game_issue)
-        user_response = cli.get_user_response()
-        correct_answer = check_response(user_response, username, game_response)
-        print(correct_answer)
-        i = i + 1
-        if correct_answer != 'Correct!':
-            i = 0
-    congratulate(username)
+    if game:
+        greet()
+        game.get_denotation()
+        username = cli.get_username()
+        i = 0
+        while i < ATTEMPTS:
+            game_issue, game_response = game.get_game()
+            ask(game_issue)
+            user_response = cli.get_user_response()
+            correct_answer = check_response(user_response, username, game_response)
+            print(correct_answer)
+            i = i + 1
+            if correct_answer != 'Correct!':
+                i = 0
+        congratulate(username)
+    else:
+        greet()
+

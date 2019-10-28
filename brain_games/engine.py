c443aaf1c38e71256dca004ed0ecbbdb7288ba24
Aleksey Danilchenko <danilchenko.aleksey94@gmail.com>
2019-10-28 23:29:17 +0700

    fix

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -7,25 +7,24 @@ GREETING = "Welcome to the Brain Games!"
 
 def run(game=None):
     greet()
-    if game:
-        game.get_denotation()
-        username = cli.get_username()
-        i = 0
-        while i < ATTEMPTS:
-            game_question, game_response = game.get_game()
-            ask(game_question)
-            user_response = cli.get_user_response()
-            correct_answer_text, correct_answer_value = check_response(
-                username,
-                user_response,
-                game_response,
-            )
-            print(correct_answer_text)
-            i = i + 1
-            if not correct_answer_value:
-                break
-            if i == ATTEMPTS:
-                congratulate(username)
+    if not game:
+        return
+    game.get_denotation()
+    username = cli.get_username()
+    for i in range(1, ATTEMPTS + 1):
+        game_question, game_response = game.get_game()
+        ask(game_question)
+        user_response = cli.get_user_response()
+        correct_answer_text, correct_answer_value = check_response(
+            username,
+            user_response,
+            game_response,
+        )
+        print(correct_answer_text)
+        if not correct_answer_value:
+            break
+        if i == ATTEMPTS:
+            congratulate(username)
 
 
 def greet():

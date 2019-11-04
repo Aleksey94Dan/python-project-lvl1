d9b4aabe6f397b00a6978758009259c40d15c4f1
Aleksey94Dan <danilchenko.aleksey94@gmail.com>
2019-11-04 22:08:00 +0700

    Edit get_user_response() and get_username()

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -3,10 +3,10 @@ import prompt
 
 def get_username():
     username = prompt.string('May I have your name? ')
-    print(f'Hello, {username}!\n')
+    print(f'Hello, {username}!')
+    print()
     return username
 
 
 def get_user_response():
-    user_response = prompt.string('Your answer: ').lower()
-    return user_response
+    return prompt.string('Your answer: ').lower()
